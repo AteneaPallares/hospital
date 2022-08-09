e9b930cfb28d6f9de89c3a2fbe9f3e86cc75ac82
IsraelCastro997 <israelcastro997@gmail.com>
2022-08-09 15:39:43 -0500

    general push

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Reliese\Coders\CodersServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
+        // Barryvdh\DomPDF\ServiceProvider::class,
         
 
         /*
